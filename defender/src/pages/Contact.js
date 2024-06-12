11f1ada19b87104e05095e93e3d548f3ab42d55a
Jake Bram <jhb94@txstate.edu>
2024-06-12 15:04:49 -0500

    more navigation

--- defender/src/pages/Contact.js
+++ defender/src/pages/Contact.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Navigation from '../components/Navigation';
+import './Home.css';
 
 function Contact() {
   return (
@@ -11,6 +12,10 @@ function Contact() {
       <Header />
       <h2>Contact Us</h2>
       <p>Get in touch with us for any queries.</p>
+      <p>Phone: 512-887-ALPR</p>
+      <p>PO Box: 9600 Escarpment Blvd.</p>
+      <p>Suite 745-224</p>
+      <p>Austin, Texas, 78749</p>
       <Navigation />
       <Footer />
     </div>
