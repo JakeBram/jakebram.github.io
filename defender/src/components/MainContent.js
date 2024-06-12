e0819ac54898d93877f448f84cfccc3f99f61c6a
Jake Bram <jhb94@txstate.edu>
2024-06-12 11:53:55 -0500

    Boilerplate
    Starting point.

---
+++ defender/src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main className="main-content">
+      <h2>Welcome to Defender</h2>
+      <p>This is a sample application to present information.</p>
+    </main>
+  );
+}
+
+export default MainContent;
