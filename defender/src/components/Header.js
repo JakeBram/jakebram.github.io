dd2805c5662cb5e543045e4f7e391e3ec62aa2f9
Jake Bram <jhb94@txstate.edu>
2024-06-20 11:24:50 -0500

    fixed header formatting

--- defender/src/components/Header.js
+++ defender/src/components/Header.js
@@ -1,13 +1,23 @@
+// components/Header.js
+
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../small_logo.png'; // Adjust the path if necessary
-import "./Header.css";
+import './Header.css'; // Import the CSS file
+import '../global.css'; 
 
 function Header() {
   return (
-    <header className="header">
-      <img src={logo} alt="Logo" className="logo-left" />
-      <h1 className="title">Defender Police Supply</h1>
-    </header>
+    <div className="header-container">
+      <header className="header">
+        <div className="logo-container">
+          <Link to="/">
+            <img src={logo} alt="Defender Logo" className="logo" />
+          </Link>
+        </div>
+        <h1 className="title">Defender</h1>
+      </header>
+    </div>
   );
 }
 
