d83aefbe89536ed4942acc3864570acc438a7da3
Jake Bram <jhb94@txstate.edu>
2024-06-20 14:57:44 -0500

    fixed contacts page

---
+++ defender/src/components/ContactContent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./ContactContent.css";
+
+function ContactContent() {
+  return (
+    <main className="contact-content">
+      <h2>Contact Us</h2>
+      <p>Get in touch with us for any queries.</p>
+      <p>Phone: 512-887-ALPR</p>
+      <p>PO Box: 9600 Escarpment Blvd.</p>
+      <p>Suite 745-224</p>
+      <p>Austin, Texas, 78749</p>
+    </main>
+  );
+}
+
+export default ContactContent;
--- defender/src/pages/Contact.js
+++ defender/src/pages/Contact.js
@@ -4,17 +4,13 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Navigation from '../components/Navigation';
+import ContactContent from '../components/ContactContent';
 
 function Contact() {
   return (
     <div>
       <Header />
-      <h2>Contact Us</h2>
-      <p>Get in touch with us for any queries.</p>
-      <p>Phone: 512-887-ALPR</p>
-      <p>PO Box: 9600 Escarpment Blvd.</p>
-      <p>Suite 745-224</p>
-      <p>Austin, Texas, 78749</p>
+      <ContactContent />
       <Navigation />
       <Footer />
     </div>
