0dd03171c3e4db153b73ed7b166f8d413a417659
Jake Bram <jhb94@txstate.edu>
2024-07-09 13:22:31 -0500

    fixed spelling

--- defender/src/components/MainContent.js
+++ defender/src/components/MainContent.js
@@ -5,7 +5,7 @@ function MainContent() {
   return (
     <main className="main-content">
       <h2>Welcome to Defender</h2>
-      <p>Your source for ALPR and tactical equipmet for Law Enforcement</p>
+      <p>Your source for ALPR and tactical equipment for Law Enforcement</p>
     </main>
   );
 }
