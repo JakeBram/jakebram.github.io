71217e23476f6cdff26592e66b5a6e80c0d242fa
Jake Bram <jhb94@txstate.edu>
2024-07-09 17:16:18 -0500

    added company email

--- defender/src/components/ContactContent.js
+++ defender/src/components/ContactContent.js
@@ -7,6 +7,7 @@ function ContactContent() {
       <h2>Contact Us</h2>
       <p>Get in touch with us for any queries.</p>
       <p>Phone: 512-887-ALPR</p>
+      <p>Email: jason@defenderpolicesupply.com</p>
       <p>PO Box: 9600 Escarpment Blvd.</p>
       <p>Suite 745-224</p>
       <p>Austin, Texas, 78749</p>
