3a2e56fe065ef2eef86bbc5730d0bf9fa3fe969b
Jake Bram <jhb94@txstate.edu>
2024-08-29 14:31:43 -0500

    update homepage

--- defender/src/components/products/RTS.js
+++ defender/src/components/products/RTS.js
@@ -8,7 +8,7 @@ const RTS = () => {
       <img src={arloLogo} alt="RTS Logo" className="product-logo" />
       <div className="product-text">
         <h2>RTS</h2>
-        <p>We carry ballistic panels and sheilds as well as body armor from RTS Tactical.</p>
+        <p>We carry ballistic panels and shields as well as body armor from RTS Tactical.</p>
       </div>
     </div>
   );
